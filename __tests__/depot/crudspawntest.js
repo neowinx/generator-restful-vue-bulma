2138554fcfa19279d76075dff3507c3b8b64f43e
Pedro Flores <pflores@dncp.gov.py>
2019-07-19 16:21:38 -0400

    added some depot files to easy testing

---
+++ __tests__/depot/crudspawntest.js
@@ -0,0 +1,71 @@
+const { spawn } = require("child_process");
+
+const ls = spawn("yo", ["restful-vue-bulma:crud"]);
+
+var expectedAnswers = 0;
+
+// STDOUT events
+ls.stdout.on("data", data => {
+  console.log(`stdout: ${data}`);
+  if (data.indexOf("Ingrese la url del servicio RESTful a construir las vistas crud") > 0 && expectedAnswers === 0) {
+    ls.stdin.write("\n");
+    expectedAnswers++;
+  }
+  if (data.indexOf("El servicio requiere cabecera de autencicación (JWT, Basic, etc.)") > 0  && expectedAnswers === 1) {
+    ls.stdin.write("\n");
+    expectedAnswers++;
+  }
+  if (data.indexOf("Ingrese la cabecera de autorización del servicio") > 0 && expectedAnswers === 2) {
+    ls.stdin.write("\n");
+    expectedAnswers++;
+  }
+  if (data.indexOf("Ingrese el nombre del servicio") > 0 && expectedAnswers === 3) {
+    ls.stdin.write("\n");
+    expectedAnswers++;
+  }
+  if (expectedAnswers >= 4) {
+    ls.stdin.end();
+  }
+});
+
+ls.stdout.on("error", error => {
+  console.log(`stdout error: ${error}`);
+});
+
+ls.stdout.on("close", data => {
+  console.log(`stdout close: ${data}`);
+});
+
+ls.stdout.on("end", data => {
+  console.log(`stdout end: ${data}`);
+});
+
+// STDERR events
+ls.stderr.on("data", data => {
+  console.log(`stderr: ${data}`);
+});
+
+ls.stderr.on("error", error => {
+  console.log(`stderr error: ${error}`);
+});
+
+ls.stderr.on("close", data => {
+  console.log(`stderr close: ${data}`);
+});
+
+ls.stderr.on("end", data => {
+  console.log(`stderr end: ${data}`);
+});
+
+// ChildProcess events
+ls.on("close", code => {
+  console.log(`child process closed with code ${code}`);
+});
+
+ls.on("exit", code => {
+  console.log(`child process exited with code ${code}`);
+});
+
+ls.on("disconnect", code => {
+  console.log(`child process disconnected with code ${code}`);
+});
