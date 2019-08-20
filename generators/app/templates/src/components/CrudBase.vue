8b9208ce1890809a6152afb0eeb5000cd40506ba
Pedro Flores <pflores@dncp.gov.py>
2019-08-20 13:49:42 -0400

    confirm dialog for delete

--- generators/app/templates/src/components/CrudBase.vue
+++ generators/app/templates/src/components/CrudBase.vue
@@ -10,28 +10,21 @@ export default {
     }
   },
   methods: {
-    update: function(instance) {
-      console.log('update')
-      this._put(`/user/${instance.id}`, instance)
-    },
-    save: function (instance) {
-      this._post('/user', instance, res => this.userList.push(res.data))
-    },
     _post(url, instance, callback) {
       this._request(url, 'post', instance, callback)
     },
     _put(url, instance, callback) {
-      console.log('_put')
       this._request(url, 'put', instance, callback)
     },
+    _delete(url, callback) {
+      this._request(url, 'delete', undefined, callback)
+    },
     _request(url, method, instance, callback) {
-      console.log('_request')
       HTTP.request({
         method: method,
         url: url,
         data: instance
       }).then(res => {
-        console.log(`res => ${res}`)
         if(callback) callback(res)
         this.status = 'list'
       }).catch(error => {
---
+++ generators/app/templates/src/components/dialogs/ConfirmDialogButton.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  .confirm-dialog
+    button.button(:class='[ btnCls ? btnCls : "is-primary" ]' :disabled='disabled' @click.prevent='active = true') {{ buttonTitle }}
+    .modal(:class='{ "is-active": active }')
+      .modal-background
+      .modal-card
+        header.modal-card-head
+          p.modal-card-title {{ title }}
+        section.modal-card-body
+          p {{ message }}
+        footer.modal-card-foot
+          button.button(:class='[ btnCls ? btnCls : "is-primary" ]' @click.prevent='confirm') Confirmar
+          button.button.is-info(@click.prevent='close') Cerrar
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      active: false,
+    }
+  },
+  props: {
+    title: null,
+    buttonTitle: null,
+    btnCls: null,
+    message: null,
+    disabled: false
+  },
+  methods: {
+    confirm: function () {
+      this.active = false
+      this.$emit('confirmed')
+    },
+    close: function () {
+      this.active = false
+    }
+  }
+}
+</script>
