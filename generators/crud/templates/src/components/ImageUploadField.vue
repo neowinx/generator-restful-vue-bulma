7db1f4d4a1ca221f0a57afcc82025be89f8e3cf7
Pedro Flores <pdavidflores@gmail.com>
2020-09-12 02:56:18 -0400

    ImageUpload with preview component added

--- generators/crud/templates/src/components/ImageUploadField.vue
+++ generators/crud/templates/src/components/ImageUploadField.vue
@@ -4,14 +4,16 @@
       span(v-if="required" style="color: red; margin-right: .25em") *
       | {{ label }}
     .control
-      .file
+      .file.is-centered
         label.file-label
-          input.file-input(type='file', name='resume')
+          figure.image.is-128x128
+            img(:src='preview')
+          input.file-input(type='file' accept="image/png, image/jpeg" multiple=false :name='label' @change='changed')
           span.file-cta
             span.file-icon
               font-awesome-icon(:icon="['fas', 'file-upload']")
             span.file-label
-              | Choose a file
+              | Suba un archivo
 
 </template>
 <script>
@@ -20,10 +22,27 @@
   export default {
     name: "ImageUploadField",
     extends: InputField,
+    computed: {
+      preview () {
+        if (this.value)
+          return this.value.startsWith('data:image') ? this.value : 'data:image/png;base64,' + this.value
+      }
+    },
     props: {
       type: {
         type: String,
         default: 'image'
+      }
+    },
+    methods: {
+      changed (e) {
+        let _this = this
+        if (e.target.files.length > 0) {
+          let f = e.target.files[0];
+          let reader = new FileReader();
+          reader.onload = (e2) => _this.$emit('input', e2.target.result);
+          reader.readAsDataURL(f);
+        }
       }
     }
   }
